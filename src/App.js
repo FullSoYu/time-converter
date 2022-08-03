a9fd0bbacdb7fd3fad9abb23363119b7d5e779f8
FullSoYu <hey6410@naver.com>
2022-08-03 10:16:02 +0900

    15강 active toggle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
+import { act } from "react-dom/test-utils";
 
 function App() {
   const [minutes, setMinutes] = useState(0);
+  const [active, setActive] = useState(false);
+
+  const reset = () => {
+    setMinutes(0);
+  };
+
+  const onChange = (event) => {
+    setMinutes(event.target.value);
+  };
+
+  const changeActive = () => {
+    setActive(!active);
+  };
 
   return (
     <div>
@@ -12,18 +26,21 @@ function App() {
           placeholder="Minutes"
           value={minutes}
           type="number"
-          onChange={(event) => {
-            setMinutes(event.target.value);
-          }}
+          onChange={onChange}
+          disabled={!active}
         />
         <div>
-          Hours
+          Hours{" "}
           <input
             type="number"
             placeholder="Hours"
             value={Math.floor(minutes / 60)}
+            disabled={active}
+            onChange={onChange}
           />
         </div>
+        <button onClick={reset}>reset</button>
+        <button onClick={changeActive}>toggle</button>
       </div>
     </div>
   );
